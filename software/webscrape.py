74edccf98ea716f8665e63d2eb7cb3880574f77a
Steve Tchana <sttcha31@gmail.com>
2023-11-04 10:12:30 -0400

    transfer

--- software/webscrape.py
+++ software/webscrape.py
@@ -1,4 +1,5 @@
 import selenium
 #https://web1.ncaa.org/stats/StatsSrv/careersearch
 #https://www.kaggle.com/datasets/benwieland/nba-draft-data
+#https://www.sports-reference.com/cbb/players/kevin-durant-1.html
 
